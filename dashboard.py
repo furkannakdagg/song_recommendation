5f978e61d6fa2a441e101998afc924f8a285094d
furkannakdagg <43972848+furkannakdagg@users.noreply.github.com>
2022-11-16 19:26:54 +0300

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,6 @@ import spotify_search as ss
 import extras
 from PIL import Image
 
-
 qr = Image.open('images/qr.png')
 spo_png = Image.open("images/spotify.png")
 error_img = Image.open("images/error.jpeg")
@@ -17,9 +16,11 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
 )
+
+
 def markdown_summary(col):
-    return  col.markdown(f"""
-    Sanatçı Adı: {art_info} 
+    return col.markdown(f"""
+    Sanatçı Adı: {art_info}
 
     Şarkı Adı: {song_info}
 
@@ -32,8 +33,10 @@ def markdown_summary(col):
     Sanatçının Spotify Linki: {art_spot}
     """)
 
+
 st.markdown("<h1 style='text-align: center;'>Miuul Şarkı Tavsiye Sistemi</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; font-size:18px;'>Sıradaki Favori Şarkınızı Bulun! 🎸</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; font-size:18px;'>Sıradaki Favori Şarkınızı Bulun! 🎸</h3>",
+            unsafe_allow_html=True)
 st.markdown(" ")
 col1, col2, col3 = st.columns(3)
 with col1:
@@ -46,8 +49,6 @@ with col3:
     col3.image(qr, width=200)
 st.write(" ")
 
-
-
 col1, col2 = st.columns(2, gap="medium")
 col1.markdown("""
 Aramak istediğiniz şarkıyı lütfen aşağıya yazınız 👇
@@ -55,71 +56,71 @@ Aramak istediğiniz şarkıyı lütfen aşağıya yazınız 👇
 song_name = col1.text_input("Şarkı Adı")
 artist_name = col1.text_input("Sanatçı Adı")
 
-if st.button("Şarkıyı Bul"):
+if col1.checkbox("Şarkıyı Bul"):
     if ss.search_pic(song_name, artist_name) == -1:
-        col2.markdown("""
-        ### Eminiz ki istediğiniz şarkı şu an bir yerlerde besteleniyordur 🎶
-        ### Bira ve bebek bezinizi alın, arkanıza yaslanıp yayınlanmasını bekleyip 🍻🍼
-        """)
-        col2.image(error_img, width=300)
+            col2.markdown("""
+                ### Eminiz ki istediğiniz şarkı şu an bir yerlerde besteleniyordur 🎶
+                ### Bira ve bebek bezinizi alın, arkanıza yaslanıp yayınlanmasını bekleyip 🍻🍼
+                """)
+            col2.image(error_img, width=300)
     else:
-        img, preview = ss.search_pic(song_name, artist_name)
-        col2.markdown("## Albüm Kapağı")
-        col2.image(img)
-        col2.markdown("## Şarkının Ön İzlemesi")
-        col2.audio(preview)
-        art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(song_name, artist_name)
-        markdown_summary(col1)
-        check = col1.checkbox("Şarkının doğru olduğunu onaylıyor musunuz?")
-        if song_name and artist_name and check:
-            try:
-                rec_song = df[(df["name"].str.contains(song_name, case=False)) &
-                              (df["artists"].str.contains(artist_name, case=False))] \
-                    .drop(["name", "artists"], axis=1)
-                rec_song = rec_song.iloc[0].squeeze()
-            except:
-                st.write("Aradığınız şarkı database'de bulunamadı. Spotify database'ine bağlanılıyor...")
-                st.write("Spotify database'inden şarkı bilgileri alınıyor...")
-                rec_song = ss.audio_features(song_name, artist_name)
-                st.write("Şarkı bilgileri alındı!")
-            finally:
-                tickers = ["Öneri Sayısı Seç", 3, 5, 10]
-                selection = st.selectbox("Kaç öneri görmek istersiniz?", tickers)
-                if selection != "Öneri Sayısı Seç":
-                    st.write("İşlem internet hızınıza göre 1-2 dakika sürebilmektedir! ⏱")
-                    rec_list = df.corrwith(rec_song, axis=1, numeric_only=True).sort_values(ascending=False).head(
-                        selection + 1)
+            img, preview = ss.search_pic(song_name, artist_name)
+            col2.markdown("## Albüm Kapağı")
+            col2.image(img)
+            col2.markdown("## Şarkının Ön İzlemesi")
+            col2.audio(preview)
+            art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(song_name, artist_name)
+            markdown_summary(col1)
+            col1.markdown("""#### Lütfen kaç öneri getirmek istediğinizi seçin 👇""")
+            st.write("İşlem internet hızınıza göre 1-2 dakika sürebilmektedir! ⏱")
+            tickers = ["Öneri Sayısı Seç", 3, 5, 10]
+            selection = st.selectbox("Kaç öneri görmek istersiniz?", tickers)
+            if song_name and artist_name:
+                try:
+                    rec_song = df[(df["name"].str.contains(song_name, case=False)) &
+                                  (df["artists"].str.contains(artist_name, case=False))] \
+                        .drop(["name", "artists"], axis=1)
+                    rec_song = rec_song.iloc[0].squeeze()
+                    rec_list = df.corrwith(rec_song, axis=1, numeric_only=True).sort_values(ascending=False).head(11)
                     rec_list = rec_list[1:]
                     rec_df = df.loc[rec_list.index, ["name", "artists"]]
-                    if selection != 10:
-                        rec_cols = st.columns(selection, gap="small")
-                        for i in range(rec_df.shape[0]):
-                            rec_name = rec_df.iloc[i, 0]
-                            rec_song = rec_df.iloc[i, 1]
-                            rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
-                            rec_cols[i].image(rec_img)
-                            rec_cols[i].audio(rec_preview)
-                            art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(rec_song, rec_name)
-                            markdown_summary(rec_cols[i])
+                except:
+                    rec_song = ss.audio_features(song_name, artist_name)
+                finally:
+                    if selection != "Öneri Sayısı Seç":
+                        if selection != 10:
+                            rec_cols = st.columns(selection, gap="small")
+                            for i in range(rec_df.shape[0]):
+                                rec_name = rec_df.iloc[i, 0]
+                                rec_song = rec_df.iloc[i, 1]
+                                rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
+                                rec_cols[i].image(rec_img)
+                                rec_cols[i].audio(rec_preview)
+                                art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(rec_song,
+                                                                                                           rec_name)
+                                markdown_summary(rec_cols[i])
 
-                    elif selection == 10:
-                        rec_list = df.corrwith(rec_song, axis=1).sort_values(ascending=False).head(selection + 1)
-                        rec_list = rec_list[~(rec_list == 1)]
-                        rec_df = df.loc[rec_list.index, ["name", "artists"]]
-                        rec_cols = st.columns(5, gap="small")
-                        for i in range(5):
-                            rec_name = rec_df.iloc[i, 0]
-                            rec_song = rec_df.iloc[i, 1]
-                            rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
-                            rec_cols[i].image(rec_img)
-                            rec_cols[i].audio(rec_preview)
-                            markdown_summary(rec_cols[i])
-                        rec_cols = st.columns(10, gap="small")
-                        for i in range(5, rec_df.shape[0]):
-                            rec_name = rec_df.iloc[i, 0]
-                            rec_song = rec_df.iloc[i, 1]
-                            rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
-                            rec_cols[i].image(rec_img)
-                            rec_cols[i].audio(rec_preview)
-                            markdown_summary(rec_cols[i])
-
+                        elif selection == 10:
+                            rec_list = df.corrwith(rec_song, axis=1).sort_values(ascending=False).head(selection + 1)
+                            rec_list = rec_list[1:]
+                            rec_df = df.loc[rec_list.index, ["name", "artists"]]
+                            rec_cols = st.columns(5, gap="small")
+                            for i in range(5):
+                                rec_name = rec_df.iloc[i, 0]
+                                rec_song = rec_df.iloc[i, 1]
+                                rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
+                                rec_cols[i].image(rec_img)
+                                rec_cols[i].audio(rec_preview)
+                                art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(rec_song,
+                                                                                                           rec_name)
+                                markdown_summary(rec_cols[i])
+                            rec_cols2 = st.columns(5, gap="small")
+                            for i in range(5):
+                                rec_name = rec_df.iloc[i+5, 0]
+                                rec_song = rec_df.iloc[i+5, 1]
+                                rec_img, rec_preview = ss.search_pic(rec_name, rec_song)
+                                rec_cols2[i].image(rec_img)
+                                rec_cols2[i].audio(rec_preview)
+                                art_info, song_info, release_date, explicit, song_spot, art_spot = ss.info(rec_song,
+                                                                                                           rec_name)
+                                markdown_summary(rec_cols2[i])
